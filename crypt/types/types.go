d66826607ce52fb4b1a8250a4144444b688fc3c0
dyaksa <diasnour0395@gmail.com>
2024-07-16 14:56:10 +0700

    feat: inserting tag data

--- crypt/types/types.go
+++ crypt/types/types.go
@@ -13,6 +13,7 @@ type (
 	AEADBool    = sqlval.AEAD[bool, crypt.PrimitiveAEAD]
 	AEADFloat64 = sqlval.AEAD[float64, crypt.PrimitiveAEAD]
 	AEADTime    = sqlval.AEAD[time.Time, crypt.PrimitiveAEAD]
+	AEADInt64   = sqlval.AEAD[int64, crypt.PrimitiveAEAD]
 	BIDXString  = sqlval.BIDX[string, crypt.PrimitiveBIDX]
 	HMACString  = sqlval.HMAC[string, crypt.PrimitiveHMAC]
 )
